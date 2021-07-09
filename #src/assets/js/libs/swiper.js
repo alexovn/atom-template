65f65f301565f6463a70c83b4bf65c14ddae6960
Nikita Alexov <arcticisland@protonmail.com>
2021-07-09 10:37:24 +0500

    update core files

--- #src/assets/js/libs/swiper.js
+++ #src/assets/js/libs/swiper.js
@@ -1,8 +1,5 @@
-import Swiper JS
 import Swiper from 'swiper';
-core version + navigation, pagination modules:
 import SwiperCore, { Navigation, Pagination } from 'swiper/core';
-configure Swiper to use modules
 SwiperCore.use([Navigation, Pagination]);
 
 // const swiper = new Swiper('.swiper-container', {
