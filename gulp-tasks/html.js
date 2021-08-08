380251e7b52a3180daf151d0fcba8eb16641cf46
Nikita Alexov <arcticisland@protonmail.com>
2021-08-08 12:31:36 +0500

    html prettify and favicons path changes

--- gulp-tasks/html.js
+++ gulp-tasks/html.js
@@ -7,6 +7,8 @@ import plumber from "gulp-plumber";
 import webphtml from "gulp-webp-in-html";
 import environments from "gulp-environments"
 import nunjucks from "gulp-nunjucks";
+import prettify from 'gulp-prettify';
+
 const browserSync = require("browser-sync").create();
 
 const development = environments.development;
@@ -20,6 +22,12 @@ function html() {
             path: [srcPath + "views/**/*.html"]
         }))
         .pipe(production(webphtml()))
+        .pipe(prettify( {
+            indent_size: 2,
+            preserve_newlines: false,
+            end_with_newline: true,
+            wrap_attributes: 'auto'
+        }))
         .pipe(dest(path.build.html))
         .pipe(browserSync.stream());
 }
