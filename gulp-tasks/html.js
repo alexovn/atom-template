6f3bd1489f15c7741e40f24f17ec5a5c547aa54f
Nikita Alexov <arcticisland@protonmail.com>
2021-08-11 23:06:38 +0500

    code review and cleaning

--- gulp-tasks/html.js
+++ gulp-tasks/html.js
@@ -5,14 +5,15 @@ import { src, dest } from "gulp";
 
 import plumber from "gulp-plumber";
 import webphtml from "gulp-webp-in-html";
-import environments from "gulp-environments"
 import nunjucks from "gulp-nunjucks";
 import prettify from 'gulp-prettify';
+import gulpif from "gulp-if";
 
 const browserSync = require("browser-sync").create();
 
-const development = environments.development;
-const production = environments.production;
+import yargs from 'yargs';
+const argv = yargs.argv;
+const production = !!argv.production;
 
 
 function html() {
@@ -21,8 +22,8 @@ function html() {
         .pipe(nunjucks.compile({
             path: [srcPath + "views/**/*.html"]
         }))
-        .pipe(production(webphtml()))
-        .pipe(prettify( {
+        .pipe(gulpif(production, webphtml()))
+        .pipe(prettify({
             indent_size: 2,
             preserve_newlines: false,
             end_with_newline: true,
